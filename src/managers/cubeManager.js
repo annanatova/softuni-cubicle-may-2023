26fa51c8697b67ad99ba8a3015d8a0cb0d067345
annanatova <114179638+annanatova@users.noreply.github.com>
2023-05-25 21:32:38 +0200

    Add cube details page

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -3,6 +3,7 @@ const uniqid = require('uniqid');
 const cubes = [];
 
 exports.getAll = () => cubes.slice();
+exports.getOne = (cubeId) => cubes.find(x => x.id == cubeId);
 
 exports.create = (cubeData) => {
     const newCube = {
