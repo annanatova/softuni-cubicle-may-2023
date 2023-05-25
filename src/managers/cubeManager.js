ab770d94cd4129d78cd6b3ef133c1e9c806df55d
annanatova <114179638+annanatova@users.noreply.github.com>
2023-05-25 20:39:33 +0200

    Add id generator

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,12 +1,12 @@
 // cubeData = { name, description, imageUrl, difficultyLevel }
-
+const uniqid = require('uniqid');
 const cubes = [];
 
 exports.getAll = () => cubes.slice();
 
 exports.create = (cubeData) => {
     const newCube = {
-        id: cubes.length + 1,
+        id: uniqid(),
         ...cubeData,
     };
     cubes.push(newCube);
