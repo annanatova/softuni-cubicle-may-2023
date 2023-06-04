8e14018bd1e2afab8211b9f965e85f38abc4b799
annanatova <114179638+annanatova@users.noreply.github.com>
2023-06-04 23:50:50 +0200

    Add relation from cube to accessory

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,5 +1,6 @@
 // cubeData = { name, description, imageUrl, difficultyLevel }
 
+const Accessory = require('../models/Accessory.js');
 const Cube = require('../models/Cube');
 
 exports.getAll = async (search, from, to) => {
@@ -19,8 +20,15 @@ exports.getAll = async (search, from, to) => {
 }
 exports.getOne = (cubeId) => Cube.findById(cubeId);
 
-exports.create = async (cubeData) => {
+exports.create = (cubeData) => {
    const cube = new Cube(cubeData);
-   await cube.save();
-   return cube;
+   return cube.save();
 };
+
+exports.attachAccessory = async (cubeId, accessoryId) => {
+    // return Cube.findByIdAndUpdate(cubeId, {$push: {accessories: accessoryId} });
+    const cube = await Cube.findById(cubeId);
+    cube.accessories.push(accessoryId);
+    return cube.save();
+
+}
