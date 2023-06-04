519ad58e16454b009c8cd53a23f8593d908aa34a
annanatova <114179638+annanatova@users.noreply.github.com>
2023-06-04 21:02:02 +0200

    Add post action for create accessory

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -2,6 +2,12 @@ const router = require('express').Router();
 
 router.get('/create', (req, res) => {
     res.render('accessory/create');
-})
+});
+
+router.post('/create', (req, res) => {
+    const body = req.body;
+    // TODO: Add accessory data to database
+    res.redirect('/')
+});
 
 module.exports = router;
