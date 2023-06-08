76fa632fd1f8211e7ac75c161ee48305b8bd7be6
annanatova <114179638+annanatova@users.noreply.github.com>
2023-06-08 22:17:41 +0200

    Hash password before save

--- src/models/User.js
+++ src/models/User.js
@@ -1,15 +1,10 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: String,
     password: {
         type: String,
-        // validate: {
-        //     validator: function(value) {
-        //         return this.repeatPassword === value;
-        //     },
-        //     message: `Password missmatch!`,
-        // },
     },
 });
 
@@ -18,6 +13,11 @@ userSchema.virtual('repeatPassword')
     if(value !== this.password) {
         throw new mongoose.MongooseError('Password missmatch!');
     }
+});
+
+userSchema.pre('save', async function() {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
 });
 
 const User = mongoose.model('User', userSchema);
