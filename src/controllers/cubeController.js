d4d8a2436234c914f6820a7e45c25e4f2fbdb08d
annanatova <114179638+annanatova@users.noreply.github.com>
2023-06-10 00:00:51 +0200

    Add generate options for difficultyLevel

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const cubeManager = require("../managers/cubeManager");
 const accessoryManager = require('../managers/accessoryManager');
+const { getDifficultyOptionsViewData } = require('../utils/viewHelpers');
 
 // Path /cubes/create
 router.get("/create", (req, res) => {
@@ -49,7 +50,8 @@ router.post('/:cubeId/attach-accessory', async (req, res) => {
 
 router.get('/:cubeId/delete', async (req, res) => {
   const cube = await cubeManager.getOne(req.params.cubeId).lean();
-  res.render('cube/delete', { cube });
+  const options = getDifficultyOptionsViewData(cube.difficultyLevel);
+  res.render('cube/delete', { cube, options });
 });
 
 router.post('/:cubeId/delete', async (req, res) => {
@@ -59,7 +61,8 @@ res.redirect('/');
 
 router.get('/:cubeId/edit', async (req, res) => {
   const cube = await cubeManager.getOne(req.params.cubeId).lean();
-  res.render('cube/edit', { cube });
+  const options = getDifficultyOptionsViewData(cube.difficultyLevel);
+  res.render('cube/edit', { cube, options });
 });
 
 router.post('/:cubeId/edit', async (req, res) => {
