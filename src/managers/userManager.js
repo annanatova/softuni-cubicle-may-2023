7423c874e75d2f87ee095eaecaf460362931912d
annanatova <114179638+annanatova@users.noreply.github.com>
2023-06-09 21:00:59 +0200

    Create token and set to cookie

--- src/managers/userManager.js
+++ src/managers/userManager.js
@@ -1,5 +1,9 @@
 const bcrypt = require('bcrypt');
+const jwt = require('../lib/jwt');
 const User = require('../models/User');
+
+// node -e "console.log(require('crypto').randomBytes(32).toString('hex'))"
+const SECRET = 'bbf908f59c60c3604a605341e54139fd24ca6477ddb7d43e873b2ec18846c40b';
 
 exports.register = (userData) => User.create(userData);
 exports.login = async (username, password) => {
@@ -13,5 +17,11 @@ exports.login = async (username, password) => {
         throw new Error('Cannot find user or password!');
     }
     
-    return user;
+    const payload = {
+        _id: user._id,
+        username: user.username,
+    }
+    const token = await jwt.sign(payload, SECRET, { expiresIn: '2d'});
+
+    return token;
 }
